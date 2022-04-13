a8ac554876720dc4c3a954ddc480363af068b296
YanisHlali <78662780+YanisHlali@users.noreply.github.com>
2022-04-13 14:09:57 +0200

    Add files via upload

--- pages/api/distances.js
+++ pages/api/distances.js
@@ -7,10 +7,14 @@ export default async function(req, res) {
         modelsDistance.afficherDistance()
         .then(response => {
           if (response != "") {
-            res.json(response)
+            let test;
+            for (let i = 0; i < 200; i++) {
+              test += "<item>"+response[i].nom_commune+"</item>\n"
+            }
+            console.log(test)
+            res.send(test)
           }
         })
-        // Test
         .catch(error => {
           res.json(error); 
           res.status(405).end();
