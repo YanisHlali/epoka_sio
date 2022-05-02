5e53ce568b886cd19546fa34f95b2cdf17289b7e
YanisHlali <78662780+YanisHlali@users.noreply.github.com>
2022-05-02 23:56:48 +0200

    Debut ajout de missions

--- models/modelsMission.js
+++ models/modelsMission.js
@@ -37,6 +37,8 @@ agences.id_agence=salaries.idAgence AND `;
 
 async function creerMission(debut,fin,idJournaliste,commune) {
     return new Promise((resolve,reject) => {
+        console.log(`INSERT INTO missions (debut_mission,fin_mission,status_mission,estValider_mission,estPayer_mission,
+            idJournaliste_mission,idCommune_mission) VALUES ("${debut}","${fin}","En attente...",0,0,${idJournaliste},${commune})`)
         db.query(`INSERT INTO missions (debut_mission,fin_mission,status_mission,estValider_mission,estPayer_mission,
         idJournaliste_mission,idCommune_mission) VALUES ("${debut}","${fin}","En attente...",0,0,${idJournaliste},${commune})`, (err,result) => {
             if (err) throw err;
