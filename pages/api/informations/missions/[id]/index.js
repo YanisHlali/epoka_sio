eaf5f2c6166d1e2245d65b7ee89b7139be73147b
YanisHlali <78662780+YanisHlali@users.noreply.github.com>
2022-06-03 10:03:42 +0200

    Add files via upload

--- pages/api/informations/missions/[id]/index.js
+++ pages/api/informations/missions/[id]/index.js
@@ -2,7 +2,7 @@ const modelsMission = require('../../../../../models/modelsMission')
 
 export default async function(req,res) {
     return new Promise((resolve,reject) => {
-        modelsMission.getMissions(req.query.id)
+        modelsMission.getMissions(3)
         .then(result => {
             res.json(result)
         })
