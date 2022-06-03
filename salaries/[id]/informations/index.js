492ce48b177c8bca0e86500dbb3f08307c5a691b
YanisHlali <78662780+YanisHlali@users.noreply.github.com>
2022-06-03 09:41:24 +0200

    Add files via upload

--- salaries/[id]/informations/index.js
+++ salaries/[id]/informations/index.js
@@ -2,7 +2,7 @@ const modelsMission = require('../../../../../models/modelsMission')
 
 export default async function(req,res) {
     return new Promise((resolve,reject) => {
-        modelsMission.getMissionBySalarie(req.query.id)
+        modelsMission.getMissionBySalarie(3)
         .then(result => {
             resolve(result);
         })
