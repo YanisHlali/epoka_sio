e2d913ddf044248c30bdff01a439ca6fdd63ebd6
YanisHlali <78662780+YanisHlali@users.noreply.github.com>
2022-06-03 09:44:19 +0200

    Add files via upload

--- salaries/[id]/informations/index.js
+++ salaries/[id]/informations/index.js
@@ -1,10 +1,14 @@
 const modelsMission = require('../../../../../models/modelsMission')
+const db = require('../../../../../models/models')
 
 export default async function(req,res) {
     return new Promise((resolve,reject) => {
-        modelsMission.getMissionBySalarie(3)
-        .then(result => {
+        db.query(`SELECT * FROM missions,communes,salaries
+        WHERE missions.idJournaliste_mission=salaries.id_salarie AND
+        missions.idCommune_mission=communes.id_commune AND
+        id_salarie=${3} AND NOW()-fin_mission < 90`, (err,result) => {
+            if (err) throw err;
             resolve(result);
-        })
+        });
     })
 }
