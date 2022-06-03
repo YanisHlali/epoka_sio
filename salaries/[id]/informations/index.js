7dbf39d3fa366577b5127e2e0312222d3136a64d
YanisHlali <78662780+YanisHlali@users.noreply.github.com>
2022-06-03 09:39:17 +0200

    Add files via upload

---
+++ salaries/[id]/informations/index.js
@@ -0,0 +1,10 @@
+const modelsMission = require('../../../../../models/modelsMission')
+
+export default async function(req,res) {
+    return new Promise((resolve,reject) => {
+        modelsMission.getMissionBySalarie(req.query.id)
+        .then(result => {
+            resolve(result);
+        })
+    })
+}
